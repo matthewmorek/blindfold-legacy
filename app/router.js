04576c4c0fbc8fe05bc72f5da7ea58f0b7adaf1b
Matthew Morek <matthew@madbit.co>
2018-03-23 14:32:06 +0000

    🐛 Fix incorrect check for `config.env` variable

--- app/router.js
+++ app/router.js
@@ -37,7 +37,7 @@ module.exports.init = (app, config) => {
     {
       consumerKey: config.app_key,
       consumerSecret: config.app_secret,
-      callbackURL: 'http://' + config.site_host + (config.env === 'production' ? ':' + config.port : '') + '/auth/callback'
+      callbackURL: 'http://' + config.site_host + (config.env !== 'production' ? ':' + config.port : '') + '/auth/callback'
     },
     function (token, tokenSecret, profile, cb) {
       return cb(null, profile, {
